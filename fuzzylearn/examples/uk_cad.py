8e6ff8fdfc9e464f1f05d7e6b0cdb8ce973207a3
Hossein Javedani Sadaei <h.javedani@gmail.com>
2023-05-22 22:01:52 -0700

    add plots

---
+++ fuzzylearn/examples/uk_cad.py
@@ -0,0 +1,148 @@
+from fuzzylearn.classification.fast.fast import FLfastClassifier
+from sklearn.metrics import classification_report,confusion_matrix,f1_score,roc_auc_score
+from feature_engine.imputation import CategoricalImputer, MeanMedianImputer
+from category_encoders import OrdinalEncoder
+from sklearn.pipeline import Pipeline
+from feature_engine.selection import DropConstantFeatures
+import pandas as pd
+import time
+import xgboost
+import optuna
+from optuna.samplers import TPESampler
+from optuna.pruners import HyperbandPruner
+from zoish.feature_selectors.shap_selectors import ShapFeatureSelector
+from sklearn.model_selection import train_test_split
+from fuzzylearn.util.read_data import read_data_from_gdrive_or_local
+
+data = read_data_from_gdrive_or_local('UKB_CAD')
+data = data[0:1000]
+print(data.columns)
+print(data.shape)
+
+cols_to_drop =[
+    'eid'
+       
+]
+
+# drop some columns
+
+
+data["label"] = data["DX_Coronary_artery_disease"].astype(int)
+
+# # Train test split
+
+data.drop(cols_to_drop, errors='ignore', inplace=True, axis='columns')
+print(data.columns)
+
+
+X = data.loc[:, data.columns != "label"]
+y = data.loc[:, data.columns == "label"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, stratify=y["label"], random_state=42
+)
+
+
+print(X_train.head())
+print(X_test.head())
+
+print(X_train.columns)
+print(X_test.columns)
+
+print(y_train.columns)
+print(y_test.columns)
+
+int_cols = X_train.select_dtypes(include=["int"]).columns.tolist()
+float_cols = X_train.select_dtypes(include=["float"]).columns.tolist()
+cat_cols = X_train.select_dtypes(include=["object"]).columns.tolist()
+
+
+print('int_cols')
+print(int_cols)
+print('float_cols')
+print(float_cols)
+print('cat_cols')
+print(cat_cols)
+
+
+pipeline =Pipeline([
+            # drop constant features
+            #('dropconstantfeatures',DropConstantFeatures(tol=0.8, missing_values='ignore')),
+            # int missing values imputers
+            ('intimputer', MeanMedianImputer(
+                imputation_method='median', variables=int_cols)),
+            ('floatimputer', MeanMedianImputer(
+                imputation_method='mean', variables=float_cols)),
+
+ ])
+
+X_train = pipeline.fit_transform(X_train,y_train)
+X_test = pipeline.transform(X_test)
+
+
+start_time = time.time()
+model = FLfastClassifier(number_of_intervals=15,threshold=0.7,metric = 'euclidean').fit(X=X_train,y=y_train,X_valid=None,y_valid=None)
+print("--- %s seconds for training ---" % (time.time() - start_time))
+
+start_time = time.time()
+y_pred = model.predict(X=X_test)
+print("--- %s seconds for prediction ---" % (time.time() - start_time))
+
+print("classification_report :")
+print(classification_report(y_test, y_pred))
+print("confusion_matrix : ")
+print(confusion_matrix(y_test, y_pred))
+print("roc_auc_score : ")
+print(roc_auc_score(y_test, y_pred))
+print("f1_score : ")
+print(f1_score(y_test, y_pred))
+
+
+
+
+int_cols = X_train.select_dtypes(include=["int"]).columns.tolist()
+float_cols = X_train.select_dtypes(include=["float"]).columns.tolist()
+cat_cols = X_train.select_dtypes(include=["object"]).columns.tolist()
+
+
+print('int_cols')
+print(int_cols)
+print('float_cols')
+print(float_cols)
+print('cat_cols')
+print(cat_cols)
+
+
+pipeline_selected_features =Pipeline([
+            # drop constant features
+            #('dropconstantfeatures',DropConstantFeatures(tol=0.8, missing_values='ignore')),
+            # int missing values imputers
+            ('intimputer', MeanMedianImputer(
+                imputation_method='median', variables=int_cols)),
+            ('floatimputer', MeanMedianImputer(
+                imputation_method='mean', variables=float_cols)),
+
+ ])
+
+X_train = pipeline_selected_features.fit_transform(X_train,y_train)
+X_test = pipeline_selected_features.transform(X_test)
+
+
+start_time = time.time()
+model = FLfastClassifier(number_of_intervals='freedman',threshold=0.7,metric = 'manhattan').fit(X=X_train,y=y_train,X_valid=None,y_valid=None)
+print("--- %s seconds for training ---" % (time.time() - start_time))
+
+start_time = time.time()
+y_pred = model.predict(X=X_test)
+model.feature_improtance()
+print("--- %s seconds for prediction ---" % (time.time() - start_time))
+
+print("classification_report :")
+print(classification_report(y_test, y_pred))
+print("confusion_matrix : ")
+print(confusion_matrix(y_test, y_pred))
+print("roc_auc_score : ")
+print(roc_auc_score(y_test, y_pred))
+print("f1_score : ")
+print(f1_score(y_test, y_pred))
